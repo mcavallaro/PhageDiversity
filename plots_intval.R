72e672d2720fd156cf5ab4d752ff510ef9737528
fabfreund <fafreund_popgen@gmx.net>
2025-12-02 00:14:52 +0000

    updated plot for int val

--- plots_intval.R
+++ plots_intval.R
@@ -3,25 +3,25 @@ library(janitor) #rounding up at 0.5
 library(tidyverse)
 
 #' Load data, bring into 
-#' tibble form, only second 100 
-load("intval_n100_train4_allres_2025.RData")
-res25 <- bind_cols(valid_res[[1]][101:200,],
+#' tibble form, only second half 
+load("intval_n500_train5_rawdist_2025.RData")
+res25 <- bind_cols(valid_res[[1]][501:1000,],
             host=rep(names(valid_res)[1],
             nrow(valid_res[[1]])/2))
 for (i in 2:length(valid_res)){
   res25 <- bind_rows(res25,
-              bind_cols(valid_res[[i]][101:200,],
+              bind_cols(valid_res[[i]][501:1000,],
                         host=rep(names(valid_res)[i],
                         nrow(valid_res[[i]])/2)))
 }
-#' Only retain 100 NAEs
-load("intval_n100_train4_allres_2024.RData")
-res24 <- bind_cols(valid_res[[1]][101:200,],
+#' Only retain the NAEs
+load("intval_n500_train5_rawdist_2024.RData")
+res24 <- bind_cols(valid_res[[1]][501:1000,],
           host=rep(names(valid_res)[1],
                    nrow(valid_res[[1]])/2))
 for (i in 2:length(valid_res)){
   res24 <- bind_rows(res24,
-                     bind_cols(valid_res[[i]][101:200,],
+                     bind_cols(valid_res[[i]][501:1000,],
                                host=rep(names(valid_res)[i],
                                       nrow(valid_res[[i]])/2)))
 }
@@ -36,17 +36,34 @@ plotd <- plotd |>  mutate(stat = fct_relevel(stat,
                                              c("GT","ET","PYP","FPG")))
 
 #' Doublecheck: only >2 for GT
-plotd |> filter(value>2) |> select(stat) |> table()
+#plotd |> filter(value>2) |> select(stat) |> table()
 #' Set values to at most 2
-plotd$value <- sapply(plotd$value,function(x){min(2,x)})
+#plotd$value <- sapply(plotd$value,function(x){min(2,x)})
 
-ggplot(data = plotd,
+#' As GT is bad for lambda>1 we make 2 plots - 
+#' one for lamba <=1 with all statistics and one for all
+#' lambda for all but GT
+
+#' plot for small lambda
+ggplot(data = plotd |> filter(!training %in% c("0.25","0.35")),
        aes(x=training,y=value,fill=stat)) + 
   geom_boxplot() + labs(title = 2024,
                         x="% training",
                         y="NAE") + 
   facet_wrap(vars(host),scales="free_y")
-ggsave("intval_2024.pdf")
+ggsave("intval_2024_gt.pdf")
+
+#' plot for lambda > 1, no GT
+ggplot(data = plotd |> 
+         filter(!stat == "GT",
+                training %in% c("0.25","0.35")),
+       aes(x=training,y=value,fill=stat)) + 
+  geom_boxplot() + labs(title = 2024,
+                        x="% training",
+                        y="NAE") + 
+  facet_wrap(vars(host),scales="free_y")
+ggsave("intval_2024_nogt_smalltrain.pdf")
+
 
 #2025
 
@@ -60,26 +77,43 @@ plotd <- plotd |>  mutate(stat = fct_relevel(stat,
 plotd <- plotd |>  filter(training!="0.pred")
 
 #' Doublecheck: only >2 for GT
-plotd |> filter(value>2) |> select(stat) |> table()
+#plotd |> filter(value>2) |> select(stat) |> table()
 
 #' Set values to at most 2
-plotd$value <- sapply(plotd$value,function(x){min(2,x)})
+#plotd$value <- sapply(plotd$value,function(x){min(2,x)})
 
-ggplot(data = plotd,
+ggplot(data = plotd |>
+         filter(!training %in% c("0.25","0.35")),
        aes(x=training,y=value,fill=stat)) + 
   geom_boxplot() + labs(title = 2025,
                         x="% training",
                         y="NAE") + 
   facet_wrap(vars(host),scales="free_y")
-ggsave("intval_2025.pdf")
+ggsave("intval_2025_gt.pdf")
 
-ggplot(data = plotd,
-       aes(x=host,y=value,fill=training)) + 
+#' plot for lambda > 1, no GT
+ggplot(data = plotd |> 
+         filter(!stat == "GT",
+                training %in% c("0.25","0.35")),
+       aes(x=training,y=value,fill=stat)) + 
   geom_boxplot() + labs(title = 2025,
                         x="% training",
                         y="NAE") + 
-  facet_wrap(vars(stat),scales="free_y")
-ggsave("intval_2025_reord.pdf")
+  facet_wrap(vars(host),scales="free_y")
+ggsave("intval_2025_nogt_smalltrain.pdf")
+
+
+ggplot(data = plotd |> filter(stat!="GT"),
+       aes(x=host,y=value,fill=training)) + 
+  geom_boxplot() + labs(title = 2025,
+                        x="% training",
+                        y="NAE") + theme() + 
+  facet_wrap(vars(stat),
+             scales="free_y") +
+theme(axis.text.x = element_text(angle = 90))
+ggsave("intval_2025_bytraining.pdf")
+
+
 
 ggplot(data = plotd,
        aes(x=training,y=value,fill=stat)) + 
@@ -88,6 +122,6 @@ ggplot(data = plotd,
                         y="NAE") + 
   lims(y=c(0,0.2)) +
   facet_wrap(vars(host),scales="free_y")
-ggsave("intval_2025_zoom.pdf")
+#ggsave("intval_2025_zoom.pdf")
 
 
