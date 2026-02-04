309ec651d0f6b29fbf9e42ef93002c1c968ba0c5
Cavallaro <mc811@leicester.ac.uk>
2026-02-04 22:15:00 +0000

    cover

--- predict_cover.R
+++ predict_cover.R
@@ -1,6 +1,8 @@
 library(magrittr)
 library(dplyr)
 library(tidyverse)
+library(ggplot2)
+library(ggrepel)
 # import functions for non-paramteric estimates
 source("nonparam_estimators.R")
 
@@ -20,29 +22,6 @@ nosamples_host <- sapply(spec_byhost_l, nrow)
 nospecies_host <- sapply(spec_byhost_l, function(x){nrow(unique(x))})
 samples_1K <- which(nosamples_host>999)
 
-# At this point, for each host, we have a collection of phages (individual observations, along with their species names)
-# > spec_byhost_l[["Pseudomonas"]]
-# # A tibble: 1,644 × 1
-# `Phage Species`
-# <chr>          
-# 1 vOTU_0044      
-# 2 vOTU_0044
-# 3 vOTU_0045
-# 4 vOTU_0048
-# 5 vOTU_0049
-# 6 vOTU_0048
-# 7 vOTU_0049
-# 8 vOTU_0052
-# 9 vOTU_0054
-# 10 vOTU_0056
-# #  1,634 more rows
-# #  Use `print(n = ...)` to see more rows
-
-# plot(sort(getSpeciesCount(spec_byhost_l[["Pseudomonas"]]), decreasing = T), log = 'xy')
-
-# Estimate the missing species from the Sept2024 data and see how it compares with 
-# the last dataset
-
 new_species_ET<-tibble()
 
 host_names<-c("Escherichia", "Klebsiella",
@@ -55,13 +34,14 @@ for (n1 in host_names ){
   speccounts<-getSpeciesCount(spec_byhost_l[[n1]])
   freq_table<-getFrequencyTable(speccounts)
   cat("\n", n1)
+  n_actual_species = sum(freq_table)
   cat(". n of species: ", sum(freq_table), " ", length(speccounts))
   m =  c(freq_table %*% as.numeric(names(freq_table)))
   cat(". n of isolates: ", m, " ", length(spec_byhost_l[[n1]][[1]]), " ", sum(speccounts))
   m = seq(1, m * 1.3)
 
   temp1<-efron_thisted(freq_table, m, 
-                         max_terms = 10)
+                         max_terms = 10) + n_actual_species
 
   make_est_m<-function(v){vm <- v
                           vm[1] <- max(0,v[1]) 
@@ -84,41 +64,31 @@ for (n1 in host_names ){
   t1<-t1 |> mutate(m = as.integer(m))
   t1<-t1 |> mutate(estim. = factor(estim.))
 
- #Make boostrap for different m
- bt_ET<-function(){
-   bt1<-sample(speccounts, length(speccounts), 
-               replace=T)
-   freqtab<-getFrequencyTable(bt1)
-   out1<-efron_thisted(freqtab, m, 
-                         max_terms = 10)
-   out2<-make_est_m(out1)
-   return(c(out1, out2))
-  }
-  bt_ET_df<-replicate(100, bt_ET(), simplify = TRUE)
-  bt_ET_qu<-t(apply(bt_ET_df, 1,
-        function(v){quantile(v,probs=c(0.025,0.975))}))
-
-  bt_ET_qu2<-tibble(qlow = bt_ET_qu[,1],
-                    qhigh = bt_ET_qu[,2],
-                    m2 = rep(m,2),
-                    estim. = c(rep("ET", length(m)), rep("mod. ET", length(m)))
-                    )
-  bt_ET_qu2<-bt_ET_qu2 |> rename(m=m2)
-  t1<-inner_join(t1, bt_ET_qu2, by = c("m", "estim."))
   new_species_ET<-bind_rows(new_species_ET, t1)
 }
 
-data1<-new_species_ET
-data1 |> ggplot() + geom_line(aes(x = m,y = value,
-                              colour = estim.)) +
-  geom_ribbon(aes(x = m, ymin = qlow, ymax = qhigh,
-                  colour = estim.), fill = NA,
-              alpha = 85, linetype = 3) +
-        geom_vline(aes(xintercept = n),
-                   linetype = 2
-                   ) +
-  labs(y="#predicted species") + 
-  facet_wrap(facet=vars(host),
-             nrow=4,ncol=2,scales="free")
-    
-ggsave(filename = "predict_new.pdf")
+data1<-new_species_ET[new_species_ET$estim. == 'mod. ET',]
+
+labels<-data1 %>% group_by(host) %>% slice_tail(n=1)
+
+data1 |> ggplot() + geom_line(
+    aes(x = m, y = value, colour = host)
+  ) +
+  geom_text_repel(
+    data = labels,
+    aes(x = m, y = value, label = host),
+    box.padding = 0.1,
+    point.padding = 0.5,
+    max.overlaps = Inf,
+    segment.color = 'white',
+    fontface = "italic",
+    size = 6
+  ) +
+  labs(y = "Number of phage species per host", x = "Number of future individuals observed (sampling effort)") + 
+  theme_minimal() + 
+  theme(legend.position = "none",
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 13))
+
+
+ggsave(filename = "cover.png", bg = 'white')
